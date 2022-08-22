149e6908be327e1250e11a517d96c773be9c46a4
İrem ERGİN <irem.ergin@std.medipol.edu.tr>
2022-08-22 22:13:10 +0300

    commit

---
+++ 3DES/3DES/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using static _3DES.TripleDes;
+
+namespace _3DES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen kullanmak için bir şifre girin: ");
+            string password = Console.ReadLine();
+            Console.WriteLine("Lütfen şifrelemek için bir dize girin:");
+            string plaintext = Console.ReadLine();
+            Console.WriteLine("");
+
+            Console.WriteLine("Şifrelenmiş dizeniz:");
+            string encryptedstring = StringCipher.Encrypt(plaintext, password);
+            Console.WriteLine(encryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Şifresi çözülmüş dizeniz:");
+            string decryptedstring = StringCipher.Decrypt(encryptedstring, password);
+            Console.WriteLine(decryptedstring);
+            Console.WriteLine("");
+
+            Console.WriteLine("Çıkmak için herhangi bir tuşa basın...");
+            Console.ReadLine();
+        }
+    }
+    }
+
---
+++ 3DES/3DES/TripleDes.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using System.Linq;
+namespace _3DES
+{
+    public class TripleDes
+    {
+        public static class StringCipher
+        {
+            //  Bu sabit, şifreleme algoritmasının anahtar boyutunu bit cinsinden belirlemek için kullanılır.
+            // Eşdeğer bayt sayısını elde etmek için bunu aşağıdaki kodda 8'e bölüyoruz.
+            private const int Keysize = 256;
+
+            // Bu sabit, parola bayt oluşturma işlevi için yineleme sayısını belirler.
+            private const int DerivationIterations = 1000;
+
+            public static string Encrypt(string plainText, string passPhrase)
+            {
+                // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
+                // so that the same Salt and IV values can be used when decrypting.  
+                var saltStringBytes = Generate256BitsOfRandomEntropy();
+                var ivStringBytes = Generate256BitsOfRandomEntropy();
+                var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+                using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+                {
+                    var keyBytes = password.GetBytes(Keysize / 8);
+                    using (var symmetricKey = new RijndaelManaged())
+                    {
+                        symmetricKey.BlockSize = 256;
+                        symmetricKey.Mode = CipherMode.CBC;
+                        symmetricKey.Padding = PaddingMode.PKCS7;
+                        using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+                        {
+                            using (var memoryStream = new MemoryStream())
+                            {
+                                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                                {
+                                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                                    cryptoStream.FlushFinalBlock();
+                                    // rastgele iv baytların ve şifre baytlarının bir birleşimi olarak son baytları oluşturun.
+                                    var cipherTextBytes = saltStringBytes;
+                                    cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+                                    cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+                                    memoryStream.Close();
+                                    cryptoStream.Close();
+                                    return Convert.ToBase64String(cipherTextBytes);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            public static string Decrypt(string cipherText, string passPhrase)
+            {
+                // byte akısı
+                // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
+                var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
+                // Sağlanan cipherText baytlarından ilk 32 baytı çıkararak saltbaytları alın.
+                var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
+                //Sağlanan cipherText baytlarından sonraki 32 baytı çıkararak IV baytlarını alın.
+                var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
+                //cipherText dizesinden ilk 64 baytı kaldırarak gerçek şifre metni baytlarını alın.
+                var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
+
+                using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+                {
+                    var keyBytes = password.GetBytes(Keysize / 8);
+                    using (var symmetricKey = new RijndaelManaged())
+                    {
+                        symmetricKey.BlockSize = 256;
+                        symmetricKey.Mode = CipherMode.CBC;
+                        symmetricKey.Padding = PaddingMode.PKCS7;
+                        using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                        {
+                            using (var memoryStream = new MemoryStream(cipherTextBytes))
+                            {
+                                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                                {
+                                    var plainTextBytes = new byte[cipherTextBytes.Length];
+                                    var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                                    memoryStream.Close();
+                                    cryptoStream.Close();
+                                    return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            private static byte[] Generate256BitsOfRandomEntropy()
+            {
+                var randomBytes = new byte[32]; // 32 Byte bize 256 bit verecektir.
+                using (var rngCsp = new RNGCryptoServiceProvider())
+                {
+                    // Diziyi kriptografik olarak güvenli rastgele baytlarla doldurun.
+                    rngCsp.GetBytes(randomBytes);
+                }
+                return randomBytes;
+            }
+        }
+    }
+}
